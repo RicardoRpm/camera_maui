7dcd0aadabb79d5b4f5103585e07925062d3af0a
RicardoRpm <Ricardomiguel190@gmail.com>
2026-01-04 15:17:17 +0000

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using camera_maui.Views;
+
+namespace camera_maui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void CallCamera_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new CameraView());
+        }
+    }
+}
---
+++ Views/CameraView.xaml.cs
@@ -0,0 +1,11 @@
+
+
+namespace camera_maui.Views;
+
+public partial class CameraView : ContentPage
+{
+	public CameraView()
+	{
+		InitializeComponent();
+	}
+}
