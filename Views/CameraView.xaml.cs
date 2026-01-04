fd8f5a24795a47c51610494e271b7eeaa28df38e
RicardoRpm <Ricardomiguel190@gmail.com>
2026-01-04 16:52:49 +0000

    call cameraview w

--- Views/CameraView.xaml.cs
+++ Views/CameraView.xaml.cs
@@ -1,5 +1,7 @@
 
 
+using CommunityToolkit.Maui.Core;
+
 namespace camera_maui.Views;
 
 public partial class CameraView : ContentPage
@@ -8,4 +10,21 @@ public partial class CameraView : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void cameraView_MediaCaptured(object sender, CommunityToolkit.Maui.Core.MediaCapturedEventArgs e)
+    {
+
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        var resultImage = await cameraView.CaptureImage(CancellationToken.None);
+
+
+    }
+
+    private void cameraView_Loaded(object sender, EventArgs e)
+    {
+        
+    }
 }
